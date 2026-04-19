0ba7020aab639b24770227b0d016a257c1db7170
anaayaar-ops <ana.ayaar@gmail.com>
2026-04-19 09:40:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ service.on('ready', async () => {
     
     const targetGroup = 18432094;
     const totalEvents = 32;
-    let startTime = new Date(2026, 3, 21, 21, 0, 0); // تبدأ من 12:00 AM يوم 18 فبراير
+    let startTime = new Date(2026, 3, 22, 21, 0, 0); // تبدأ من 12:00 AM يوم 18 فبراير
     const surveyRecords = [];
 
     try {
